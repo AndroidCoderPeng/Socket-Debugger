3a9fd381e21a39cd9ce3ceed4ac3b9c2e87e876a
pengxianhong <290677893@qq.com>
2022-05-05 21:36:06 +0800

    调整界面布局

--- Socket Debugger/View/MainWindow.xaml.cs
+++ Socket Debugger/View/MainWindow.xaml.cs
@@ -1,68 +1,15 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Media.Imaging;
-using Socket_Debugger.Utils;
+﻿using System.Windows;
 
 namespace Socket_Debugger.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
-        private readonly string[] _itemImages =
-        {
-            @"Resources/Image/Tcp.png",
-            @"Resources/Image/Tcp.png",
-            @"Resources/Image/Udp.png",
-            @"Resources/Image/Udp.png",
-            @"Resources/Image/WebSocket.png",
-            @"Resources/Image/WebSocket.png"
-        };
-
-        private readonly string[] _itemNames = {"TCP客户端", "TCP服务端", "UDP客户端", "UDP服务端", "WS客户端", "WS服务端"};
-
-        private ObservableCollection<FunctionItem> FunctionItems { get; } = new ObservableCollection<FunctionItem>();
-
         public MainWindow()
         {
             InitializeComponent();
-            //初始化数据库
-            SqLiteHelper.GetInstance().InitDataBase();
-
-            //设置左边ListView数据
-            for (int i = 0; i < 6; i++)
-            {
-                FunctionItems.Add(new FunctionItem
-                {
-                    ItemImage = new BitmapImage(new Uri(_itemImages[i], UriKind.Relative)),
-                    ItemTitle = _itemNames[i]
-                });
-            }
-
-            FuncListView.ItemsSource = FunctionItems;
-            FuncListView.SelectedIndex = 0;
-            //设置中间Panel数据
-        }
-
-        private void AddButton_Click(object sender, RoutedEventArgs e)
-        {
-            // 添加配置
-            FunctionItem selectedItem = (FunctionItem) FuncListView.SelectedItem;
-            var configDialog = new AddConfigDialog(selectedItem.ItemTitle);
-            configDialog.ShowDialog();
-        }
-
-        private void DelButton_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        public class FunctionItem
-        {
-            public BitmapImage ItemImage { get; set; }
-
-            public string ItemTitle { get; set; }
         }
     }
 }
